0ab5975fb933bd640c05f8ebc55f7ef3f1272598
James-Ude <udejames27@gmail.com>
2023-12-02 13:54:08 -0800

    correction on indentation

--- 0x03-python-data_structures/4-new_in_line.py
+++ 0x03-python-data_structures/4-new_in_line.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    if idx > len(new_list) - 1 or idx < 0:
-        return new_list
-    new_list[idx] = element
+    if not my_list:
+        return
+    new_list = my_list.copy()
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
     return new_list
